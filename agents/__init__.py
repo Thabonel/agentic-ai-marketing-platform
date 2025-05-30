d057fcd2d8a61d89b74db9da8b837196733f02a3
Thabonel <thabonel0@gmail.com>
2025-05-30 21:16:41 +1000

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,1 +1,25 @@
+# agents/__init__.py
 
+from .base_agent import BaseAgent
+from .marketing_agents import (
+    CampaignManagementAgent,
+    LeadGenerationAgent
+)
+
+# Import new agents
+from .content_agent import ContentCreationAgent
+from .social_media_agent import SocialMediaAgent
+from .analytics_agent import AnalyticsAgent
+from .email_automation_agent import EmailAutomationAgent
+
+__all__ = [
+    'BaseAgent',
+    'CampaignManagementAgent', 
+    'LeadGenerationAgent',
+    'ContentCreationAgent',
+    'SocialMediaAgent',
+    'AnalyticsAgent',
+    'EmailAutomationAgent'
+]
+
+__version__ = '2.0.0'
